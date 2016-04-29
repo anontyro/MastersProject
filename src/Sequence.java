3d24c25f009c264205b0d5306b17525c2133fc15
Alex <Alex@Alex-PC>
2016-04-29 23:31:13 +0800

    April 29th Project 1 started

--- src/Sequence.java
+++ src/Sequence.java
@@ -1,8 +1,9 @@
 
 
-public abstract class Sequence {
+public class Sequence {
     private String description = "";
     private String content = "";
+    private int sequenceLength = 0;
     
     Sequence(String description, String content){
         this.description = description;
@@ -15,5 +16,49 @@ public abstract class Sequence {
     
     public String getContent(){
         return content;
+    }
+    
+    public int getLength(){
+        return content.length();
+    }
+    
+    public boolean simpleValidate(){
+        for(int i = 0; i < getLength(); i++){
+            if(content.charAt(i)!= 'A')
+               if(content.charAt(i) != 'C')
+                   if(content.charAt(i) != 'T')
+                       if(content.charAt(i) != 'G'){
+                           return false;
+                       }
+                
+               
+            
+        }
+        return true;
+    }
+    
+    public boolean validate() throws InvalidSequenceException{
+        int index = 0;
+        try{
+            
+        for(int i = 0; i < getLength(); i++){
+            if (content.charAt(i) != ('A' | 'T' | 'G' | 'C')){
+                i = index;
+                throw (new InvalidSequenceException(content,i));
+                
+                
+            }
+            }
+        }
+        catch(InvalidSequenceException error){
+            throw (new InvalidSequenceException(content,index));
+        }
+            
+        
+        
+        
+        return true;
+        
+        
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -10,6 +10,15 @@
  */
 public class Test {
     public static void main(String[]args){
+        Sequence sq1 = new Sequence("test","ATGCATCAAAGGG");
+
+
+        System.out.println(sq1.getContent());
+        System.out.println(sq1.getLength());
+        System.out.println(sq1.simpleValidate());
         
+        
+        
+
     }
 }
